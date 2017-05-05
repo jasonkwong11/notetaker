02f596faf967952d72ca24bcf094bfd3f68cc504
Jason <jasonkwong11@gmail.com>
2017-05-05 19:34:58 -0400

    set up react-redux framework

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,8 +4,23 @@ import 'babel-polyfill'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
 
 import App from './app'
+import helloReducer from './reducer/hello'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
+import { isProd } from '../shared/util'
 
-ReactDOM.render(<App />, document.querySelector(APP_CONTAINER_SELECTOR))
+const store = createStore(combineReducers({ hello: helloReducer }),
+  //eslint-disable-next-line no-underscore-dangle
+  isProd ? undefined : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
+
+const wrapApp = (AppComponent, reduxStore) =>
+  <Provider store={reduxStore}>
+    <AppComponent />
+  </Provider>
+
+ReactDOM.render(wrapApp(App, store), rootEl)
