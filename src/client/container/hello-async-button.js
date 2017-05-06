2ff1a3039e3356105acdf133b11b495236f2b1b5
Jason <jasonkwong11@gmail.com>
2017-05-06 16:25:06 -0400

    add initial async redux architecture

---
+++ src/client/container/hello-async-button.js
@@ -0,0 +1,16 @@
+// @flow
+
+import { connect } from 'react-redux'
+import { sayHelloAsync } from '../action/hello'
+import Button from '../component/button'
+
+const mapStateToProps = () => ({
+  label: 'Say hello asynchronously and send 1234',
+})
+
+//below is a hard coded 1234 value. This value would typically come from a form filled by the user
+const mapDispatchToProps = dispatch => ({
+  handleClick: () => { dispatch(sayHelloAsync(1234)) },
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Button)
