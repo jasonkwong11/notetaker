c8cd24d935b534988986139be86898a049fa37e8
Jason <jasonkwong11@gmail.com>
2017-05-06 20:03:15 -0400

    implement server side routing

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,20 +4,25 @@ import 'babel-polyfill'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Immutable from 'immutable'
 import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { BrowserRouter } from 'react-router-dom'
 
-import App from './app'
-import helloReducer from './reducer/hello'
+import App from '../shared/app'
+import helloReducer from '../shared/reducer/hello'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 
-// eslint-disable-next-line no-underscore-dangle
+/* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+const preloadedState = window.__PRELOADED_STATE__
+/* eslint-enable no-underscore-dangle */
 
-const store = createStore(combineReducers({ hello: helloReducer }),
+const store = createStore(combineReducers(
+  { hello: helloReducer }),
+  { hello: Immutable.fromJS(preloadedState.hello) },
   composeEnhancers(applyMiddleware(thunkMiddleware)))
 
 const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
---
+++ src/server/controller.js
@@ -0,0 +1,20 @@
+// @flow
+
+export const homePage = () => null
+
+export const helloPage = () => ({
+  hello: { message: 'Server-side preloaded message' },
+})
+
+export const helloAsyncPage = () => ({
+  hello: { messageAsync: 'Server-side preloaded message for async page' },
+})
+
+export const helloEndpoint = (num: number) => ({
+  serverMessage: `Hello from the server! (received ${num})`,
+})
+
+// This would typically make business logic and database calls
+// right now, we're just hard-coding some results.
+// The results are passed back to the routing module to be used
+// to initialize our server-side Redux store
